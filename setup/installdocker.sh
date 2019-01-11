ec6c06faa8cf1bef686cdb8b0a336f593a460188
adnanjee <adnan.iftekhar.ch@gmail.com>
2019-01-11 20:14:53 +0800

    update fabric script to 1.4.0

--- setup/installdocker.sh
+++ setup/installdocker.sh
@@ -55,6 +55,13 @@
 
 	sudo usermod -aG docker $USER
 
+### Install docker compose ###
+
+	echo "# Installing Docker-Compose"
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" \
+    	-o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+
 ### Remember to log out and back in for this to take effect! ###
 
 	echo "### Please log out and login back for take effect!!! ###"
