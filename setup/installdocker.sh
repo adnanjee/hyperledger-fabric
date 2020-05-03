7903c97edb6ae6905060fd2b484ad59878bcdda4
Adnan Iftekhar <16976969+adnanjee@users.noreply.github.com>
2020-05-04 06:56:18 +0800

    Update installdocker.sh

--- setup/installdocker.sh
+++ setup/installdocker.sh
@@ -60,8 +60,7 @@
 ### Install docker compose ###
 
 	echo "# Installing Docker-Compose"
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" \
-    	-o /usr/local/bin/docker-compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
 
 ### Remember to log out and back in for this to take effect! ###
