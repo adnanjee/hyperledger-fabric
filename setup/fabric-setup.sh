591f6c718ff9e265c529f53f672adf1e7f744b1a
Adnan Iftekhar <16976969+adnanjee@users.noreply.github.com>
2020-05-04 07:00:00 +0800

    update fabric to version 2.x

--- setup/fabric-setup.sh
+++ setup/fabric-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### https://github.com/adnanjee/hyperledger-fabric-1.4.git ###
+### https://github.com/adnanjee/hyperledger-fabric.git ###
 
 export PATH=$PATH:$GOROOT/bin
 
@@ -12,7 +12,7 @@ echo "GOROOT=$GOROOT"
 
 echo "### Starting to Download Fabric ###"
 
-curl -sSL https://bit.ly/2ysbOFE | bash -s -- 1.4.2 1.4.2 0.4.15
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.1 1.4.6 0.4.18
 
 echo "### Copy the binaries from fabric-samples/bin to /usr/local/bin ###"
 
