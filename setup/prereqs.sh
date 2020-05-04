e3f13a420e4325254cd4013fd323e970fc5c792a
Adnan Iftekhar <16976969+adnanjee@users.noreply.github.com>
2020-05-04 09:56:52 +0800

    Update prereqs.sh

--- setup/prereqs.sh
+++ setup/prereqs.sh
@@ -53,11 +53,23 @@ sudo apt-get update
 echo "# Installing curl"
 sudo apt-get install -y curl
 
-# Install node
-sudo apt install -y nodejs
+# Install nvm dependencies
+echo "# Installing nvm dependencies"
+sudo apt-get -y install build-essential libssl-dev
 
-# Install npm
-sudo apt install -y npm
+# Execute nvm installation script
+echo "# Executing nvm installation script"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# Set up nvm environment without restarting the shell
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
+
+# Install node
+echo "# Installing nodeJS"
+nvm install 12.16.3
+nvm use 12
 
 # Ensure that CA certificates are installed
 sudo apt-get -y install apt-transport-https ca-certificates
