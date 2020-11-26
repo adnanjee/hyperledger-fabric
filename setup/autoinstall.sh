a5f09c3b7bc8d8c66ba6a42b3d804aa9822173e4
Adnan Iftekhar <16976969+adnanjee@users.noreply.github.com>
2020-11-27 06:15:22 +0800

    Update autoinstall.sh

--- setup/autoinstall.sh
+++ setup/autoinstall.sh
@@ -1,6 +1,6 @@
-sudo bash prereqs.sh
-sudo bash installgo.sh 
-sudo bash installdocker.sh
-sudo bash fabric-setup.sh
-sudo bash caserver-setup.sh
-sudo bash jq.sh
+bash prereqs.sh
+bash installgo.sh 
+bash installdocker.sh
+bash fabric-setup.sh
+bash caserver-setup.sh
+bash jq.sh
