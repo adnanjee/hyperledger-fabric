e36a6611ce6cd8a40917a9345e36c033dc88dcf4
Adnan Iftekhar <16976969+adnanjee@users.noreply.github.com>
2020-11-27 21:32:01 +0800

    Update prereqs.sh

--- setup/prereqs.sh
+++ setup/prereqs.sh
@@ -57,32 +57,6 @@ sudo apt-get install -y git
 
 echo "# Installing curl"
 sudo apt-get install -y curl
-
-# Install nvm dependencies
-echo "# Installing nvm dependencies"
-sudo apt-get -y install build-essential libssl-dev
-
-# Execute nvm installation script
-echo "# Executing nvm installation script"
-bash nvm.sh
-
-# Set up nvm environment without restarting the shell
-export NVM_DIR="${HOME}/.nvm"
-[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
-[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
-
-# Install node
-echo "# Installing nodeJS"
-nvm install 14.15.1
-nvm alias default 14.15.1
-
-# Ensure that CA certificates are installed
-sudo apt-get -y install apt-transport-https ca-certificates
-
-# Install python v2 and v3 
-
-   sudo apt-get install -y python
-   sudo apt-get install -y python3
    
 # Install unzip, required to install hyperledger fabric.
 sudo apt-get -y install unzip
@@ -90,18 +64,4 @@ sudo apt-get -y install unzip
 source ~/.profile
 source ~/.bashrc
 
-# Print installation details for user
-echo ''
-echo 'Installation completed, versions installed are:'
-echo ''
-echo -n 'Node:           '
-node --version
-echo -n 'npm:            '
-npm --version
-echo -n 'Python:         '
-python -V
-python3 -V
-
-# Print reminder of need to logout in order for these changes to take effect!
-echo ''
 echo "#### Done ###"
